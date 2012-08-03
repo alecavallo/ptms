5439362c67b530eeb708d4d6f6d8e3793e6da7e2
alecavallo <alejandro.cavallo@gmail.com>
2012-08-03 17:43:05 -0300

    fix categories ads

--- app/controllers/categories_controller.php
+++ app/controllers/categories_controller.php
@@ -208,9 +208,9 @@ class CategoriesController extends AppController {
 			array_multisort($ads[4]['displayed'], SORT_ASC, $ads[4]['data']);
 			
 			$ads[5]['data'] = $this->Ad->get(5,$id);
-			if(!empty($ads[1]['data']) || !empty($ads[2]['data'])){
+			//if(!empty($ads[1]['data']) || !empty($ads[2]['data'])){
 				Cache::write ( "cate_{$id}_ads", $ads, 'vLong' );
-			}
+			//}
 		}else {
 			//$ads = $this->Session->read('ads');
 		}
